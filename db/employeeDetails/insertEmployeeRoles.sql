1b95d1318a9348c3eddf91604cc9d7626e00b2b0
Henry <henry1192_2004@live.com.au>
2024-10-25 00:00:07 +1100

    push everything

---
+++ db/employeeDetails/insertEmployeeRoles.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee_roles (title, salary, department_id) VALUES
+('Sales Manager', 80000, 1),
+('Software Engineer', 90000, 2),
+('HR Specialist', 60000, 3),
+('Marketing Coordinator', 50000, 4);
---
+++ db/employeeDetails/insertEmployees.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('John', 'Doe', 3, 1),
+('Jane', 'Smith', 4, 1),
+('Emily', 'Jones', 5, 1),
+('Michael', 'Brown', 6, 1);
+
